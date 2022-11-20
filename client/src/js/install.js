6e242202035a8b7c65a50f65c51cfbe19d166184
ejackson1228 <ejackson1228@gmail.com>
2022-11-20 14:45:50 -0500

    added GET and PUT for indexedDB and started on installation of PWA logic

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,19 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); // prevent prompt initiation before install 
+    butInstall.style.visibility = 'visible';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async (event) => {
+    window.prompt();
+    butInstall.setAttribute('disabled', true); // disable install btn after click to install
+    butInstall.textContent = 'Installed!'; //once installed, let the user know with the button
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('appinstalled', event);
+});
